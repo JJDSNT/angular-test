a999d1b325619d214f45bba142fbc7bccd47bdfe
Jaime Dias <jaimejosediasnt@gmail.com>
2024-08-06 06:03:48 -0300

    feature: adicionando o componente under-construction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { TypewriterComponent } from "./typewriter/typewriter.component";
+import { UnderConstructionComponent } from "./under-construction/under-construction.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TypewriterComponent],
+  imports: [RouterOutlet, HeaderComponent, TypewriterComponent, UnderConstructionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
