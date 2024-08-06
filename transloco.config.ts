673c4f495a7a2d2254da95f53fdd3551bec2ea03
Jaime Dias <jaimejosediasnt@gmail.com>
2024-08-06 14:23:12 -0300

    feature: conserta o basepath para produção?

--- transloco.config.ts
+++ transloco.config.ts
@@ -3,7 +3,7 @@ import {TranslocoGlobalConfig} from '@jsverse/transloco-utils';
 const config: TranslocoGlobalConfig = {
   rootTranslationsPath: 'src/assets/i18n/',
   langs: [ 'en', 'pt' ],
-  keysManager: {}
+  keysManager: {},
 };
     
 export default config;
