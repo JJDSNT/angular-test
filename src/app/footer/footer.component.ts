8aa9acaafda4680c0e51d194cb98caa8e34937c0
Jaime Dias <jaimejosediasnt@gmail.com>
2024-08-07 00:19:46 -0300

    Feature: adiciona tradução ao footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslocoModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  constructor(private translocoService: TranslocoService) {}
 }
