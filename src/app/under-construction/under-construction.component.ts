ee1ff174f570fe67b17838f108b58cc67d1794ab
Jaime Dias <jaimejosediasnt@gmail.com>
2024-08-07 00:13:34 -0300

    Feature: adiciona tradução ao componente underConstruction

--- src/app/under-construction/under-construction.component.ts
+++ src/app/under-construction/under-construction.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { TranslocoService, TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-under-construction',
   standalone: true,
-  imports: [],
+  imports: [TranslocoModule],
   templateUrl: './under-construction.component.html',
   styleUrl: './under-construction.component.css'
 })
 export class UnderConstructionComponent {
-
+  constructor(private translocoService: TranslocoService) {}
 }
