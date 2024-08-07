47aa5d90472ec85fbacf463e3040e759cb7d8aab
Jaime Dias <jaimejosediasnt@gmail.com>
2024-08-07 11:33:13 -0300

    Feature: avança no scroll trigger

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, OnInit, ElementRef } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, inject } from '@angular/core';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
+import { TranslocoService } from '@jsverse/transloco';
+import { TypewriterService } from '../typewriter/typewriter.service';
 
 gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [TranslocoModule],
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
 })
@@ -18,28 +18,84 @@ export class ContentComponent implements OnInit {
   title: string = '';
   content: string = '';
 
-  constructor(
-    private elementRef: ElementRef,
-    private translocoService: TranslocoService
-  ) {}
+  private typewriterService = inject(TypewriterService);
+  private translocoService = inject(TranslocoService);
+
+  constructor(private elementRef: ElementRef) {}
 
   ngOnInit(): void {
     const el = this.elementRef.nativeElement.querySelector('.content-section');
+    const textElement =
+      this.elementRef.nativeElement.querySelector('.text-element');
+    const imageElement =
+      this.elementRef.nativeElement.querySelector('.image-element');
 
-    this.translocoService.selectTranslateObject(`content${this.index}Title`).subscribe(title => {
-      this.title = title;
-    });
-    this.translocoService.selectTranslateObject(`content${this.index}`).subscribe(content => {
-      this.content = content;
-    });
+    this.translocoService
+      .selectTranslateObject(`content${this.index}Title`)
+      .subscribe((title) => {
+        this.title = title;
+      });
+    this.translocoService
+      .selectTranslateObject(`content${this.index}`)
+      .subscribe((content) => {
+        this.content = content;
+      });
 
-    ScrollTrigger.create({
-      trigger: el,
-      start: 'top 80%',
-      onEnter: () => {
-        console.log('Element entered view:', el);
-        gsap.to(el, { opacity: 1, y: 0, duration: 1 });
-      }
-    });
+    if (textElement) {
+      gsap.fromTo(
+        textElement,
+        { opacity: 0, y: -50 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.5,
+          scrollTrigger: {
+            trigger: el,
+            start: 'top 80%',
+            end: 'top 20%',
+            toggleActions: 'play reverse play reverse',
+            onEnter: () => {
+              console.log("enviando "+this.title);
+              //this.typewriterService.setTitle(this.title);
+            },
+          },
+        }
+      );
+    }
+
+    if (imageElement) {
+      gsap.fromTo(
+        imageElement,
+        { opacity: 0, x: -50 },
+        {
+          opacity: 1,
+          x: 0,
+          duration: 0.5,
+          scrollTrigger: {
+            trigger: el,
+            start: 'top 80%',
+            end: 'top 20%',
+            toggleActions: 'play reverse play reverse',
+          },
+        }
+      );
+    }
+
+    if (el) {
+      gsap.fromTo(
+        el,
+        { opacity: 0 },
+        {
+          opacity: 1,
+          duration: 0.5,
+          scrollTrigger: {
+            trigger: el,
+            start: 'top 80%',
+            end: 'top 20%',
+            toggleActions: 'play reverse play reverse',
+          },
+        }
+      );
+    }
   }
 }
